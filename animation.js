5c7eefc491238985fd0b66b978ed33e45a3fb123
sarahsehee <limteraction@gmail.com>
2020-02-26 16:09:51 +0900

    댄생님한테 in-view.js 설명들음

--- animation.js
+++ animation.js
@@ -1,8 +1,35 @@
 
 
-inView('.section2')
-  .on('enter', glasses => {
-    const glass = document.getElementById("image1");
-    glasses.glass.classList.add('in-viewport')
+inView('.section1')
+  .on('enter', section => { 
+    document.querySelector('.glass1').classList.add('show')
+    document.querySelector('.glass2').classList.remove('show')
+    document.querySelector('.glass3').classList.remove('show')
   })
+  .on('exit', section => {
+    document.querySelector('.glass1').classList.remove('show')
+  });
 
+  inView('.section2')
+  .on('enter', section => { 
+    document.querySelector('.glass2').classList.add('show')
+    document.querySelector('.glass1').classList.remove('show')
+    document.querySelector('.glass3').classList.remove('show')
+  })
+  .on('exit', section => {
+    document.querySelector('.glass2').classList.remove('show')
+  });
+
+
+  inView('.section3')
+  .on('enter', section => { 
+    document.querySelector('.glass3').classList.add('show')
+    document.querySelector('.glass1').classList.remove('show')
+    document.querySelector('.glass2').classList.remove('show')
+  })
+  .on('exit', section => {
+    document.querySelector('.glass3').classList.remove('show')
+  });
+
+
+  inView.threshold(0.1);
