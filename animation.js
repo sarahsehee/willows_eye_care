40428cd6ab64c3c7803104e68d24991a662ae216
Sehee Lim <sarahsehee.lim@gmail.com>
2020-03-29 20:15:07 +0900

    Added Swiper Slider https://swiperjs.com/api/#layout

--- animation.js
+++ animation.js
@@ -5,20 +5,23 @@ inView('.section1')
     document.querySelector('.glass1').classList.add('show')
     document.querySelector('.glass2').classList.remove('show')
     document.querySelector('.glass3').classList.remove('show')
-  })
+  });
+  /*
   .on('exit', section => {
     document.querySelector('.glass1').classList.remove('show')
-  });
+  }); */
 
   inView('.section2')
   .on('enter', section => { 
     document.querySelector('.glass2').classList.add('show')
     document.querySelector('.glass1').classList.remove('show')
     document.querySelector('.glass3').classList.remove('show')
-  })
+  });
+  /*
   .on('exit', section => {
     document.querySelector('.glass2').classList.remove('show')
   });
+  */
 
 
   inView('.section3')
@@ -27,9 +30,26 @@ inView('.section1')
     document.querySelector('.glass1').classList.remove('show')
     document.querySelector('.glass2').classList.remove('show')
   })
+  /*
   .on('exit', section => {
     document.querySelector('.glass3').classList.remove('show')
   });
+  */
 
 
-  inView.threshold(0.1);
+  inView.threshold(0.03);
+
+
+/* Swiper Test */
+var mySwiper = new Swiper ('.swiper-container', {
+  spaceBetween: 30,
+  loop: true,
+  direction: 	'horizontal',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
