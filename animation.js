6d0d1a8d75ac34ae2fee7c4a12b7c639babe1253
sarahsehee <limteraction@gmail.com>
2020-07-28 10:26:12 +0900

    * sliderjs로 변경 * 이미지 변경

--- animation.js
+++ animation.js
@@ -1,52 +1,33 @@
-
-
-inView('.section1')
-  .on('enter', section => { 
-    document.querySelector('.glass1').classList.add('show')
-    document.querySelector('.glass2').classList.remove('show')
-    document.querySelector('.glass3').classList.remove('show')
-  });
-  /*
+inView(".section1").on("enter", (section) => {
+  document.querySelector(".glass1").classList.add("show");
+  document.querySelector(".glass2").classList.remove("show");
+  document.querySelector(".glass3").classList.remove("show");
+});
+/*
   .on('exit', section => {
     document.querySelector('.glass1').classList.remove('show')
   }); */
 
-  inView('.section2')
-  .on('enter', section => { 
-    document.querySelector('.glass2').classList.add('show')
-    document.querySelector('.glass1').classList.remove('show')
-    document.querySelector('.glass3').classList.remove('show')
-  });
-  /*
-  .on('exit', section => {
-    document.querySelector('.glass2').classList.remove('show')
-  });
-  */
-
-
-  inView('.section3')
-  .on('enter', section => { 
-    document.querySelector('.glass3').classList.add('show')
-    document.querySelector('.glass1').classList.remove('show')
-    document.querySelector('.glass2').classList.remove('show')
-  })
-  /*
-  .on('exit', section => {
-    document.querySelector('.glass3').classList.remove('show')
-  });
-  */
-
-
-  inView.threshold(0.5);
-
-
-/* Swiper Test */
-var mySwiper = new Swiper ('.swiper-container', {
-      loop: true,
-      autoHeight: true,
-      spaceBetween:10,
-      direction: 	'horizontal',
-      pagination: {
-        el: '.swiper-pagination',
-      },
+inView(".section2").on("enter", (section) => {
+  document.querySelector(".glass2").classList.add("show");
+  document.querySelector(".glass1").classList.remove("show");
+  document.querySelector(".glass3").classList.remove("show");
 });
+/*
+  .on('exit', section => {
+    document.querySelector('.glass2').classList.remove('show')
+  });
+  */
+
+inView(".section3").on("enter", (section) => {
+  document.querySelector(".glass3").classList.add("show");
+  document.querySelector(".glass1").classList.remove("show");
+  document.querySelector(".glass2").classList.remove("show");
+});
+/*
+  .on('exit', section => {
+    document.querySelector('.glass3').classList.remove('show')
+  });
+  */
+
+inView.threshold(0.5);
