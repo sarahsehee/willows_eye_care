0ea64948b3b8108e4e65c6b1cab2e54b3d2b4c44
sarahsehee <limteraction@gmail.com>
2020-07-29 16:43:20 +0900

    no message

--- animation.js
+++ animation.js
@@ -3,31 +3,30 @@ inView(".section1").on("enter", (section) => {
   document.querySelector(".glass2").classList.remove("show");
   document.querySelector(".glass3").classList.remove("show");
 });
-/*
-  .on('exit', section => {
-    document.querySelector('.glass1').classList.remove('show')
-  }); */
 
 inView(".section2").on("enter", (section) => {
   document.querySelector(".glass2").classList.add("show");
   document.querySelector(".glass1").classList.remove("show");
   document.querySelector(".glass3").classList.remove("show");
 });
-/*
-  .on('exit', section => {
-    document.querySelector('.glass2').classList.remove('show')
-  });
-  */
 
 inView(".section3").on("enter", (section) => {
   document.querySelector(".glass3").classList.add("show");
   document.querySelector(".glass1").classList.remove("show");
   document.querySelector(".glass2").classList.remove("show");
 });
-/*
-  .on('exit', section => {
-    document.querySelector('.glass3').classList.remove('show')
-  });
-  */
+
+inView("#intro").on("exit", () => {
+  document.querySelector(".logo").classList.add("scaledown");
+  document.querySelector(".set").classList.add("slideout");
+  document.querySelector(".header").classList.add("new-padding");
+  document.querySelector(".set").classList.remove("flex-column");
+});
+
+inView("#intro").on("enter", () => {
+  document.querySelector(".logo").classList.remove("scaledown");
+  document.querySelector(".set").classList.remove("slideout");
+  document.querySelector(".set").classList.add("flex-column");
+});
 
 inView.threshold(0.5);
